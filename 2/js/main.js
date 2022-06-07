3ede9c6b0476b43bf740647603fcbb6f98d066c2
Keks <keks@htmlacademy.ru>
2022-06-07 19:52:10 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,19 @@
+const getRandomInteger = (firstNumber, secondNumber) => {
+  if ((firstNumber < 0) || (secondNumber < 0)) {
+    throw new RangeError ('Диапазон может быть только положительный, включая ноль');
+  }
+  return Math.floor(Math.random() * (Math.abs(secondNumber - firstNumber) + 1)) + Math.min(firstNumber, secondNumber);
+};
+
+getRandomInteger(0, 5);
+
+/* floating point random number function  */
+const getFloatingPointRandomNumber = (firstNumber, secondNumber, numbersAfterPoint) => {
+  if ((firstNumber < 0) || (secondNumber < 0)) {
+    throw new RangeError ('Диапазон может быть только положительный, включая ноль');
+  }
+  const randomNumber = Math.random() * (Math.abs(secondNumber - firstNumber) + 1) + Math.min(firstNumber, secondNumber);
+  return randomNumber.toPrecision(numbersAfterPoint + Math.floor(randomNumber).toString().length);
+};
+
+getFloatingPointRandomNumber(4.1681515616, 15.25, 5);
