f62b9ee75a8525e4f3729b1e921006fdd66a5c53
Kate Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2022-06-07 21:49:51 +0500

    changing declaration functions to arrow functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-function getRandomInteger(firstNumber, secondNumber) {
+const getRandomInteger = (firstNumber, secondNumber) => {
   if ((firstNumber < 0) || (secondNumber < 0)) {
     throw new RangeError ('Диапазон может быть только положительный, включая ноль');
   }
   return Math.floor(Math.random() * (Math.abs(secondNumber - firstNumber) + 1)) + Math.min(firstNumber, secondNumber);
-}
+};
 
 getRandomInteger(0, 5);
 
 /* floating point random number function  */
-function getFloatingPointRandomNumber(firstNumber, secondNumber, numbersAfterPoint) {
+const getFloatingPointRandomNumber = (firstNumber, secondNumber, numbersAfterPoint) => {
   if ((firstNumber < 0) || (secondNumber < 0)) {
     throw new RangeError ('Диапазон может быть только положительный, включая ноль');
   }
   const randomNumber = Math.random() * (Math.abs(secondNumber - firstNumber) + 1) + Math.min(firstNumber, secondNumber);
   return randomNumber.toPrecision(numbersAfterPoint + Math.floor(randomNumber).toString().length);
-}
+};
 
 getFloatingPointRandomNumber(4.1681515616, 15.25, 5);
