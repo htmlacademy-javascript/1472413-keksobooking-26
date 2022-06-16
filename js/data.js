8eced9c4b251f2d1bcf903a64662e9c0695dbecf
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2022-06-16 22:56:14 +0300

    adding module

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 import {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement, getRandomArray} from './util.js';
 
-const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
@@ -28,7 +28,7 @@ const createObject = (index) => {
       title: 'Сногшибательное предложение!',
       address: `${latitude}, ${longitude}`,
       price: getRandomPositiveInteger(INTEGER_MIN, INTEGER_MAX),
-      type: getRandomArrayElement(TYPE),
+      type: getRandomArrayElement(TYPES),
       rooms: getRandomPositiveInteger(INTEGER_MIN, INTEGER_MAX),
       guests: getRandomPositiveInteger(INTEGER_MIN, INTEGER_MAX),
       checkin: getRandomArrayElement(TIMES),
---
+++ js/popup.js
@@ -0,0 +1,7 @@
+import {createObjects} from './data.js';
+
+const popup = document.querySelector('#card').content.querySelector('.popup');
+const popupFragment = document.querySelector('#map-canvas');
+console.log(popup);
+
+popupFragment.appendChild();
