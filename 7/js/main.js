26d464462d5edb6c5663d6145fa11d6b62cddb33
Keks <keks@htmlacademy.ru>
2022-06-28 07:39:21 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,9 @@
+import './popup.js';
+import {adForm, mapFilter, disableForm, enableForm} from './form.js';
+import './form-validation.js';
+
+disableForm(adForm);
+enableForm(adForm);
+
+disableForm(mapFilter);
+enableForm(mapFilter);
