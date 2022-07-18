7f9c0cce3ee92a28981d0581f2ffff004cc5ad16
Keks <keks@htmlacademy.ru>
2022-07-18 21:14:47 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,20 @@
+import { renderSimilarList, mapFilter } from './map.js';
+import { setUserFormSubmit } from './form-validation.js';
+import { enableForm } from './form.js';
+import { createGetError } from './messages.js';
+import { getData } from './api.js';
+import { onFilterChange } from './form-filter.js';
+import { debounce } from './util.js';
+
+getData(
+  (objects) => {
+    renderSimilarList(objects);
+    onFilterChange(debounce(() => renderSimilarList(objects)));
+  },
+  (message) => {
+    enableForm(mapFilter, false);
+    createGetError(message);
+  }
+);
+
+setUserFormSubmit();
